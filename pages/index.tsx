7064cb834335625e4f777f772531f736d237b101
julioborn <juliobornes10@gmail.com>
2024-01-29 23:48:11 -0300

    Cambio a box

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ const Home: React.FC<Props> = ({ products }) => {
   );
 
   return (
-    <Stack spacing={6 as number | string}>
+    <Box>
       <Grid gridGap={2} templateColumns="repeat(auto-fit, minmax(200px, 1fr))">
         {products.map((product) => (
           <Stack display="flex" borderRadius="md" marginBottom="10px" padding={4} backgroundColor="yellow.200" key={product.id}>
@@ -213,7 +213,7 @@ const Home: React.FC<Props> = ({ products }) => {
           </DrawerBody>
         </DrawerContent>
       </Drawer>
-    </Stack>
+    </Box>
   );
 };
 
