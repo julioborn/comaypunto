1c460a10bfe29971feb83d681d82dcbacf55c6f9
julioborn <juliobornes10@gmail.com>
2024-02-06 21:21:04 -0300

    Volvemos a la doble columna

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,51 +104,53 @@ const Home: React.FC<Props> = ({ products }) => {
       <Grid gridGap={2} templateColumns="repeat(auto-fit, minmax(150px, 1fr))">
         {products.map((product) => (
           <Stack
-          display="flex"
-          borderRadius="md"
-          marginBottom="10px"
-          padding={2}
-          backgroundColor="yellow.200"
-          key={product.id}
-          minHeight="150px"  // Establece un tamaño mínimo para cada elemento
-        >
-          <Image maxHeight={130} borderRadius={5} objectFit="cover" src={product.imagen} alt="" />
-          <Text fontSize="20px" fontWeight="700" minHeight="30px"> {product.producto}</Text>
-          <Text fontSize="17px" fontWeight="400" minHeight="50px">{product.descripcion}</Text>
-          <Text fontSize="17px" fontWeight="500">$ {product.precio}</Text>
-          <Select
-            backgroundColor="white"
-            value={selectedOptions[product.id] || 'Normal'}
-            onChange={(e) => setSelectedOptions({ ...selectedOptions, [product.id]: e.target.value as 'Normal' | 'Doble' | 'Triple' })}
-            minHeight="30px" // Establece un tamaño mínimo para el Select
+            display="flex"
+            borderRadius="md"
+            marginBottom="10px"
+            padding={2}
+            backgroundColor="yellow.200"
+            key={product.id}
           >
-            <option value="Normal">Normal</option>
-            <option value="Doble">Doble ($150)</option>
-            {/* @ts-ignore */}
-            <option value="Triple">Triple ($200)</option>
-          </Select>
-          <Button
-            colorScheme="primary"
-            onClick={() => {
-              {/* @ts-ignore */ }
-              addToCart(product, selectedOptions[product.id] || 'Normal');
-              Swal.fire({
-                title: 'Producto agregado',
-                text: 'Puedes verlo en el carrito verde',
-                icon: 'success',
-                confirmButtonColor: '#D69E2E',
-                timer: 2500,
-                hideClass: {
-                  popup: 'animate__animated animate__fadeOutTopRight',
-                },
-              });
-            }}
-            minHeight="30px" // Establece un tamaño mínimo para el botón
-          >
-            Agregar
-          </Button>
-        </Stack>
-        
+            <Image height="130px" borderRadius={5} objectFit="cover" src={product.imagen} alt="" />
+            <Text fontSize="20px" fontWeight="700"> 
+              {product.producto}
+            </Text>
+            <Text fontSize="15px" fontWeight="400" height="110px"> {/* Establece la altura para el texto */}
+              {product.descripcion}
+            </Text>
+            <Text fontSize="17px" fontWeight="500"> 
+              $ {product.precio}
+            </Text>
+            <Select
+              backgroundColor="white"
+              value={selectedOptions[product.id] || 'Normal'}
+              onChange={(e) => setSelectedOptions({ ...selectedOptions, [product.id]: e.target.value as 'Normal' | 'Doble' | 'Triple' })}
+            >
+              <option value="Normal">Normal</option>
+              <option value="Doble">Doble ($150)</option>
+              {/* @ts-ignore */}
+              <option value="Triple">Triple ($200)</option>
+            </Select>
+            <Button
+              colorScheme="primary"
+              onClick={() => {
+                {/* @ts-ignore */ }
+                addToCart(product, selectedOptions[product.id] || 'Normal');
+                Swal.fire({
+                  title: 'Producto agregado',
+                  text: 'Puedes verlo en el carrito verde',
+                  icon: 'success',
+                  confirmButtonColor: '#D69E2E',
+                  timer: 2500,
+                  hideClass: {
+                    popup: 'animate__animated animate__fadeOutTopRight',
+                  },
+                });
+              }}
+            >
+              Agregar
+            </Button>
+          </Stack>
         ))}
       </Grid>
 
